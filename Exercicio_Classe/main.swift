ffddb750fdde7d57efd165cf0c627ddee45b52b1
Usuário Convidado <Guest@pa06micro10.local>
2019-02-27 08:17:47 -0300

    Initial Commit

---
+++ Exercicio_Classe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exercicio_Classe
+//
+//  Created by Usuário Convidado on 27/02/19.
+//  Copyright © 2019 william. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
